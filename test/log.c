19d02e58b43d9ba4ac0f485558df2c528108c308
Martin Wilck <mwilck@suse.com>
2020-05-22 01:47:33 +0200

    test/log: add test for setting log level

--- test/log.c
+++ test/log.c
@@ -52,16 +52,42 @@ static void test_log_default(void **state __attribute__((unused)))
 	log(LOG_DEBUG + 2, "not called");
 }
 
+static void test_invalid_level(void **state __attribute__((unused)))
+{
+	char buf[8];
+	int old_lvl = __ioc_loglevel;
+
+	snprintf(buf, sizeof(buf), "%s", "bad");
+	setenv("LIBIOC_LOGLEVEL", buf, true);
+	libioc_init();
+	assert_int_equal(__ioc_loglevel, old_lvl);
+
+	snprintf(buf, sizeof(buf), "%d", LOG_EMERG - 1);
+	setenv("LIBIOC_LOGLEVEL", buf, true);
+	libioc_init();
+	assert_int_equal(__ioc_loglevel, old_lvl);
+
+	snprintf(buf, sizeof(buf), "%d", MAX_LOGLEVEL + 1);
+	setenv("LIBIOC_LOGLEVEL", buf, true);
+	libioc_init();
+	assert_int_equal(__ioc_loglevel, old_lvl);
+
+	*buf = '\0';
+	setenv("LIBIOC_LOGLEVEL", buf, true);
+	libioc_init();
+	assert_int_equal(__ioc_loglevel, old_lvl);
+}
+
 static void test_log_max(void **state __attribute__((unused)))
 {
 	char buf[2];
 
-	snprintf(buf, sizeof(buf), "%d", LOG_DEBUG + 1);
+	snprintf(buf, sizeof(buf), "%d", MAX_LOGLEVEL);
 	setenv("LIBIOC_LOGLEVEL", buf, true);
 
 	libioc_init();
 
-	expect_any(__wrap_clock_gettime, clk_id);
+	expect_value(__wrap_clock_gettime, clk_id, CLOCK_MONOTONIC);
 	expect_string(__wrap_fprintf, format, IOC_LOG_TIME_FMT "max");
 	log(MAX_LOGLEVEL, "max");
 	log(MAX_LOGLEVEL + 1, "not called");
@@ -89,6 +115,7 @@ static int testsuite1(void)
 {
 	const struct CMUnitTest tests[] = {
 		cmocka_unit_test(test_log_default),
+		cmocka_unit_test(test_invalid_level),
 		cmocka_unit_test(test_log_max),
 		cmocka_unit_test(test_log),
 	};
